e524015fc0f12017c4d7c7bd4473370f47c76d0e
SiinKindred <101263795+SiinKindred@users.noreply.github.com>
2022-12-12 21:32:59 +0700

    update_phanquyen_themSp_themql

--- Database/code_phanquyen.sql
+++ Database/code_phanquyen.sql
@@ -5,7 +5,6 @@ create role Admin
 
 grant select, insert, delete, update to Admin
 
-
 -----Nhân viên
 create role NhanVien
 
@@ -24,7 +23,9 @@ grant select on NhaCungCap to NhanVien
 ---nhân viên có thêm Mã cửa hàng
 
 ---nhân viên
-create proc sp_nhanvien
+
+
+create proc sp_themnhanvien
 @tenDN nvarchar(50),
 @matkhau nvarchar(10),
 @tenNV nvarchar(50),
@@ -32,10 +33,11 @@ create proc sp_nhanvien
 @gioitinh nvarchar(3),
 @sdt int,
 @diachi nvarchar(100),
-@maCH int,
-@role nvarchar(10)
+@maCH int
 as begin
-declare @id int
+	declare @id int,
+	@role nvarchar(10)
+	set @role = 'Nhan vien'
 	if not exists (select ma_CH from CuaHang where ma_CH=@maCH)
 	begin
 		raiserror('Cửa hàng không tồn tại',16,1)
@@ -60,8 +62,40 @@ declare @id int
 	--thêm nhân viên mới
 	insert into NhanVien (ten_NV,ngaysinh,gioitinh,sdt,diachi,ma_CH,id)
 	values (@tenNV,@ngaysinh,@gioitinh,@sdt,@diachi,@maCH,@id)
-	
-	select top 1 * from NhanVien
 end
 
-exec sp_nhanvien 'DuyKha','123456','Duy Kha', 23/11/2001, 'nam', 0987654321, 'TP HCM', 1, 'Nhan Vien'
+exec sp_themnhanvien 'DuyKha','123456','Duy Kha', '20221212', 'nam', 0987654321, 'TP HCM', 1
+
+---quản lý
+create proc sp_themquanly
+@tenDN nvarchar(50),
+@matkhau nvarchar(10),
+@tenNV nvarchar(50),
+@ngaysinh datetime,
+@gioitinh nvarchar(3),
+@sdt int,
+@diachi nvarchar(100)
+as begin
+	declare @id int,
+	@role nvarchar(10)
+	set @role ='Quan ly'
+	if exists (select taikhoan from TaiKhoan where @tenDN = taikhoan)
+	begin
+		raiserror('Tài khoản đã tồn tại',16,1)
+		return
+	end
+	--tạo login
+	exec sp_addlogin @tenDN, @matkhau
+	--gán user cho login
+	exec sp_adduser @tenDN, @tenNV
+	--add user vào role nv
+	exec sp_addRolemember @role,@tenNV
+	--thêm tài khoản vào Taikhoan
+	insert into TaiKhoan(taikhoan,matkhau,role)
+	values (@tenDN,@matkhau,@role)
+	--lấy id tk vừa tạo
+	set @id = (select top 1 id from TaiKhoan where Taikhoan = @tenDN)
+	--thêm quản lý mới
+	insert into QuanLy(ten_QL,ngaysinh,gioitinh,sdt,diachi,id)
+	values (@tenNV,@ngaysinh,@gioitinh,@sdt,@diachi,@id)
+end
